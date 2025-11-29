fcc65d94ba1be4848ac322b64fae8c47d0d379f5
yurii macbook pro <yurikorock777@gmail.com>
2025-11-29 14:43:51 +0200

    feat: add to BreweryList mode, when user click on favourite (add and delete in array)

--- src/components/BreweryList/BreweryList.jsx
+++ src/components/BreweryList/BreweryList.jsx
@@ -5,13 +5,21 @@ import {
 } from "../../redux/breweries/selectors.js";
 import { useEffect, useState } from "react";
 import { fetchBreweries } from "../../redux/breweries/operations.js";
-import css from "./BreweryLIst.module.css"
+import css from "./BreweryLIst.module.css";
+import { selectFavouritesBrew } from "../../redux/favourite/favouritesSelector.js";
+import {
+  addToFavourites,
+  removeFromFavourites,
+} from "../../redux/favourite/favouritesSlice.js";
 
-export default function BreweryList() {
+export default function BreweryList({id}) {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectLoading);
   const items = useSelector(selectBreweries);
   const [page, setPage] = useState(1);
+  const favourites = useSelector(selectFavouritesBrew);
+
+  const isFavourites = favourites.includes(id);
 
   useEffect(() => {
     dispatch(fetchBreweries(page));
@@ -19,20 +27,43 @@ export default function BreweryList() {
 
   const handleLoadMore = () => setPage((prev) => prev + 1);
 
+  const handleRightClick = (event, id) => {
+    event.preventDefault();
+    if (favourites.includes(id)) {
+      dispatch(removeFromFavourites(id));
+    } else {
+      dispatch(addToFavourites(id));
+    }
+  };
+
+  const handdleDeleteAll = () => {
+    favourites.forEach((id) => dispatch(removeFromFavourites(id)));
+  };
+
   return (
     <div>
       {isLoading && <p>Loading breweries...</p>}
       <ul className={css.ul}>
-        {items.map((brewery) => (
-          <li key={brewery.id} className={css.li}>
-          <p>{brewery.name}</p>
-          <p>{brewery.brewery_type}</p>
-          <p>{brewery.city}</p>
-          </li>
-        ))}
+        {items.map((brewery) => {
+          const isSelected = favourites.includes(brewery.id);
+          return (
+            <li
+              key={brewery.id}
+              className={`${css.li} ${isSelected ? css.selected : ""}`}
+              onContextMenu={(e) => handleRightClick(e, brewery.id)}
+            >
+              <p>{brewery.name}</p>
+              <p>{brewery.brewery_type}</p>
+              <p>{brewery.city}</p>
+            </li>
+          );
+        })}
       </ul>
-      
+
       {!isLoading && <button onClick={handleLoadMore}>Load more</button>}
+      {favourites.length > 0 && (
+        <button onClick={handdleDeleteAll}>Delete</button>
+      )}
     </div>
   );
 }
